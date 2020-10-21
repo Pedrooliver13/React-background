1af045d13848e9212a6e2c5d7e5f44619272d29d
Pedro <you@example.com>
2020-10-20 21:33:07 -0300

    Fist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  return React.createElement(
+    "h1",
+    {
+      className: "title",
+    },
+    "Pedro É foda",
+  );
+}
+
+export default App;
